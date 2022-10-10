717046daedd8b99c5eba8b3c48066b713710c270
EXB00614 <matias.maruzza@claro.com.ar>
2022-10-10 15:31:06 -0300

    Updates

---
+++ precache-manifest.300d61a631b59a8950c7337c4c38c109.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "7e2dad9a2c165013ca23",
+    "url": "/madeInTuc1.0/static/css/main.04688621.chunk.css"
+  },
+  {
+    "revision": "7e2dad9a2c165013ca23",
+    "url": "/madeInTuc1.0/static/js/main.7e2dad9a.chunk.js"
+  },
+  {
+    "revision": "7b2e37571c17e8d4fdd1",
+    "url": "/madeInTuc1.0/static/js/1.7b2e3757.chunk.js"
+  },
+  {
+    "revision": "b36dd38b56f4cdfc71a2",
+    "url": "/madeInTuc1.0/static/css/2.8ac12ac3.chunk.css"
+  },
+  {
+    "revision": "b36dd38b56f4cdfc71a2",
+    "url": "/madeInTuc1.0/static/js/2.b36dd38b.chunk.js"
+  },
+  {
+    "revision": "f19cca8969ced6de2987",
+    "url": "/madeInTuc1.0/static/js/runtime~main.f19cca89.js"
+  },
+  {
+    "revision": "38f7f53872946583c70a813945508a11",
+    "url": "/madeInTuc1.0/static/media/logo.38f7f538.jpeg"
+  },
+  {
+    "revision": "7c50ead980129ea9b996cff26d2f1aa6",
+    "url": "/madeInTuc1.0/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/madeInTuc1.0/precache-manifest.300d61a631b59a8950c7337c4c38c109.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/madeInTuc1.0/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
